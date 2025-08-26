ee8b00557e1e533c9901047a84820faca8cf016e
julienexe <julianssonko@gmail.com>
2025-08-26 10:24:21 +0300

    Add facilities, participants, projects, and core apps to INSTALLED_APPS

--- collaborate/config/settings.py
+++ collaborate/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'program',
+    'facilities',
+    'participants',
+    'projects',
+    'core',
 ]
 
 MIDDLEWARE = [
