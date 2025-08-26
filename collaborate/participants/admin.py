359be7a564ec93c56e6641811b7ee7b388757a71
julienexe <julianssonko@gmail.com>
2025-08-26 10:30:35 +0300

    create admin interface entries for app models

--- collaborate/participants/admin.py
+++ collaborate/participants/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Participant, ProjectParticipant
 
-# Register your models here.
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "affiliation", "specialization", "institution")
+    search_fields = ("full_name", "email", "institution")
+    list_filter = ("affiliation", "specialization", "cross_skill_trained")
+
+@admin.register(ProjectParticipant)
+class ProjectParticipantAdmin(admin.ModelAdmin):
+    list_display = ("participant", "project", "role_on_project", "skill_role")
+    search_fields = ("participant__full_name", "project__title")
+    list_filter = ("role_on_project", "skill_role")
