ce66399f1fc9dc45eccb18463c28a9c85ae9ee04
element44 <roslerruthenium204@gmail.com>
2025-09-03 08:21:55 +0300

    admin,url,view

--- collaborate/program/urls.py
+++ collaborate/program/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path
-from . import views
+# apps/capstone/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProgramViewSet, ProjectViewSet, OutcomeViewSet
 
-app_name = 'program'
+router = DefaultRouter()
+router.register(r"programs", ProgramViewSet, basename="program")
+router.register(r"projects", ProjectViewSet, basename="project")
+router.register(r"outcomes", OutcomeViewSet, basename="outcome")
 
 urlpatterns = [
-    path('', views.ProgramListView.as_view(), name='program-list'),
-    path('<int:pk>/', views.ProgramDetailView.as_view(), name='program-detail'),
-    
+    path("api/", include(router.urls)),
 ]
+
+# config/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("apps.capstone.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
