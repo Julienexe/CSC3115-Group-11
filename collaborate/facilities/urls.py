47e53990cf951c472dc4846ac4d729a47c59ad2d
julienexe <julianssonko@gmail.com>
2025-09-03 08:59:41 +0300

    feat: add forms, views, and templates for managing facilities, services, and equipment

--- collaborate/facilities/urls.py
+++ collaborate/facilities/urls.py
@@ -4,10 +4,24 @@ from . import views
 app_name = 'facilities'
 
 urlpatterns = [
+    # Facility URLs
     path('', views.FacilityListView.as_view(), name='facility-list'),
+    path('create/', views.FacilityCreateView.as_view(), name='facility-create'),
     path('<int:pk>/', views.FacilityDetailView.as_view(), name='facility-detail'),
+    path('<int:pk>/update/', views.FacilityUpdateView.as_view(), name='facility-update'),
+    path('<int:pk>/delete/', views.FacilityDeleteView.as_view(), name='facility-delete'),
+
+    # Service URLs
     path('service/', views.ServiceListView.as_view(), name='service-list'),
+    path('service/create/', views.ServiceCreateView.as_view(), name='service-create'),
     path('service/<int:pk>/', views.ServiceDetailView.as_view(), name='service-detail'),
+    path('service/<int:pk>/update/', views.ServiceUpdateView.as_view(), name='service-update'),
+    path('service/<int:pk>/delete/', views.ServiceDeleteView.as_view(), name='service-delete'),
+
+    # Equipment URLs
     path('equipment/', views.EquipmentListView.as_view(), name='equipment-list'),
+    path('equipment/create/', views.EquipmentCreateView.as_view(), name='equipment-create'),
     path('equipment/<int:pk>/', views.EquipmentDetailView.as_view(), name='equipment-detail'),
+    path('equipment/<int:pk>/update/', views.EquipmentUpdateView.as_view(), name='equipment-update'),
+    path('equipment/<int:pk>/delete/', views.EquipmentDeleteView.as_view(), name='equipment-delete'),
 ]
--- collaborate/facilities/views.py
+++ collaborate/facilities/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Facility, Service, Equipment
+from .forms import FacilityForm, ServiceForm, EquipmentForm
 
+# Facility Views
 class FacilityListView(ListView):
     model = Facility
     template_name = "facilities/facility_list.html"
@@ -12,6 +16,31 @@ class FacilityDetailView(DetailView):
     template_name = "facilities/facility_detail.html"
     context_object_name = "facility"
 
+class FacilityCreateView(SuccessMessageMixin, CreateView):
+    model = Facility
+    form_class = FacilityForm
+    template_name = "facilities/facility_form.html"
+    success_url = reverse_lazy('facilities:facility-list')
+    success_message = "Facility '%(name)s' was created successfully"
+
+class FacilityUpdateView(SuccessMessageMixin, UpdateView):
+    model = Facility
+    form_class = FacilityForm
+    template_name = "facilities/facility_form.html"
+    success_url = reverse_lazy('facilities:facility-list')
+    success_message = "Facility '%(name)s' was updated successfully"
+
+class FacilityDeleteView(SuccessMessageMixin, DeleteView):
+    model = Facility
+    template_name = "facilities/facility_confirm_delete.html"
+    success_url = reverse_lazy('facilities:facility-list')
+    success_message = "Facility was deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
+
+# Service Views
 class ServiceListView(ListView):
     model = Service
     template_name = "facilities/service_list.html"
@@ -22,6 +51,31 @@ class ServiceDetailView(DetailView):
     template_name = "facilities/service_detail.html"
     context_object_name = "service"
 
+class ServiceCreateView(SuccessMessageMixin, CreateView):
+    model = Service
+    form_class = ServiceForm
+    template_name = "facilities/service_form.html"
+    success_url = reverse_lazy('facilities:service-list')
+    success_message = "Service '%(name)s' was created successfully"
+
+class ServiceUpdateView(SuccessMessageMixin, UpdateView):
+    model = Service
+    form_class = ServiceForm
+    template_name = "facilities/service_form.html"
+    success_url = reverse_lazy('facilities:service-list')
+    success_message = "Service '%(name)s' was updated successfully"
+
+class ServiceDeleteView(SuccessMessageMixin, DeleteView):
+    model = Service
+    template_name = "facilities/service_confirm_delete.html"
+    success_url = reverse_lazy('facilities:service-list')
+    success_message = "Service was deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
+
+# Equipment Views
 class EquipmentListView(ListView):
     model = Equipment
     template_name = "facilities/equipment_list.html"
@@ -31,3 +85,27 @@ class EquipmentDetailView(DetailView):
     model = Equipment
     template_name = "facilities/equipment_detail.html"
     context_object_name = "equipment"
+
+class EquipmentCreateView(SuccessMessageMixin, CreateView):
+    model = Equipment
+    form_class = EquipmentForm
+    template_name = "facilities/equipment_form.html"
+    success_url = reverse_lazy('facilities:equipment-list')
+    success_message = "Equipment '%(name)s' was created successfully"
+
+class EquipmentUpdateView(SuccessMessageMixin, UpdateView):
+    model = Equipment
+    form_class = EquipmentForm
+    template_name = "facilities/equipment_form.html"
+    success_url = reverse_lazy('facilities:equipment-list')
+    success_message = "Equipment '%(name)s' was updated successfully"
+
+class EquipmentDeleteView(SuccessMessageMixin, DeleteView):
+    model = Equipment
+    template_name = "facilities/equipment_confirm_delete.html"
+    success_url = reverse_lazy('facilities:equipment-list')
+    success_message = "Equipment was deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
